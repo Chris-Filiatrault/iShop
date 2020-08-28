2fff10c79b72332761fee4442faec16d554df609
Chris Filiatrault <chris Filiatrault@hotmail.com>
2020-08-28 21:56:21 +1000

    Add MKiCloudSync bridging headers & classes

---
+++ iShop/Helpers/iShop-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  iShop-Bridging-Header.h
+//  iShop
+//
+//  Created by Chris Filiatrault on 27/8/20.
+//  Copyright © 2020 Chris Filiatrault. All rights reserved.
+//
+
+#ifndef iShop_Bridging_Header_h
+#define iShop_Bridging_Header_h
+
+#import "MKiCloudSync.h"
+
+#endif /* iShop_Bridging_Header_h */
